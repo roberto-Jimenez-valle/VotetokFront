0a00935716a6388e399f07fe32ac493d569e3f95
rober <roberto.jimenezvalle@gmail.com>
2025-12-29 23:06:42 +0100

    fdg

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto solo requiere configuración mínima
-		adapter: adapter()
+		// adapter-node genera una aplicación Node.js independiente
+		adapter: adapter({
+			out: 'build'
+		})
 	},
 };
 
